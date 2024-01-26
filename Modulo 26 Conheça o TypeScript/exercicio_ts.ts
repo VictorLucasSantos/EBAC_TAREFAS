1324fe418d69030ca26e02d1fd98444d2ca9bc14
Victor Lucas Santos Gonzaga <victorlucas.santos124@gmail.com>
2024-01-26 00:20:08 -0400

    Exercicio EBAC
    A entrega deste exercício consiste em:
    
    • Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
    
    • No repositório do curso crie uma branch chamada exercicio_ts e armaze o código TS escrito nesta branch, nos envie o link através da plataforma.

---
+++ Modulo 26 Conheça o TypeScript/exercicio_ts.ts
@@ -0,0 +1,13 @@
+// Simplificado
+const Multiplicação = (Primeiro_Numero: number, Segundo_Numero: number) => Primeiro_Numero * Segundo_Numero; 
+
+const Saudação = (Nome: string) => `Olá ${Nome}!`;
+
+// Padrão
+function Multiplicacao(Primeiro_Numero: number, Segundo_Numero: number){
+    return Primeiro_Numero * Segundo_Numero
+}
+
+function Saudacao(Nome: string){
+    return `Olá ${Nome}!`
+}
