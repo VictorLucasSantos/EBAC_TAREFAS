515239501efe47cff4b5bb416e9f1eeeec514115
Victor Lucas Santos Gonzaga <victorlucas.santos124@gmail.com>
2024-06-29 20:07:49 -0400

    Adiciona arquivos de exercico módulo 7
    Adiciona arquivos de modulo 7 para resolução de tarefa do modulo

---
+++ Front-end/Modulo 7 Integrando HTML com JS/exemplos_usoJS.js
@@ -0,0 +1,19 @@
+// IMAGINE UM ARRAY EM JAVASCRIPT COMO UM TREM DE BRINQUEDO. CADA VAGÃO DO TREM É UM COMPARTIMENTO DO ARRAY, E VOCÊ PODE COLOCAR DIFERENTES BRINQUEDOS (DADOS) EM CADA VAGÃO. POR EXEMPLO, O PRIMEIRO VAGÃO PODE TER UM DINOSSAURO, O SEGUNDO UM CARRO DE CORRIDA, E ASSIM POR DIANTE. QUANDO VOCÊ QUER BRINCAR COM O CARRO DE CORRIDA, VOCÊ SÓ PRECISA IR AO SEGUNDO VAGÃO, ASSIM COMO EM UM ARRAY, VOCÊ ACESSA UM ITEM COM MEUARRAY[1] (SIM, EM ARRAYS, A CONTAGEM COMEÇA DO ZERO, ENTÃO O PRIMEIRO ITEM É [0]). ESSE TREM É UMA MANEIRA DIVERTIDA DE ORGANIZAR E ACESSAR SUAS BRINCADEIRAS (DADOS)! 🚂🧸
+
+document.getElementsByTagName('input')
+// HTMLCollection(3) [input#nome-beneficiario, input#numero-conta, input#valor, nome-beneficiario: input#nome-beneficiario, numero-conta: input#numero-conta, valor: input#valor]0: input#nome-beneficiario1: input#numero-conta2: input#valornome-beneficiario: input#nome-beneficiarionumero-conta: input#numero-contavalor: input#valorlength: 3[[Prototype]]: HTMLCollection
+let inputs = document.getElementsByTagName('input')
+// undefined
+// inputs[1]
+{/* <input type=​"number" id=​"numero-conta" required placeholder=​"Número da conta">​ */}
+document.getElementsByTagName('form')[0].getElementsByTagName('buton')
+document.getElementsByClassName('container')
+ document.querySelector('container') //-- retorna o primeiro componente
+document.querySelector('input[type=number]') //--especifica o componente
+document.querySelector('container')
+null
+// caso não tenha nada retorna nulo
+let nome_beneficiario = document.getElementById('nome-beneficiario')
+nome_beneficiario.value
+
+
