3ecfbbfecb66524a1eda918bdafd310a13657cd1
Victor Lucas Santos Gonzaga <victorlucas.santos124@gmail.com>
2024-06-29 20:10:11 -0400

    adiciona condição de igualdade

--- Front-end/Modulo 7 Integrando HTML com JS/Exercicio Modulo 7/main.js
+++ Front-end/Modulo 7 Integrando HTML com JS/Exercicio Modulo 7/main.js
@@ -7,19 +7,20 @@ const msgErro = document.querySelector('.error-message');
 
 form.addEventListener('submit', function(e){
     e.preventDefault(); // cancela a  ação padrão de submit do formular
-    
-    if (campoA.value < campoB.value){
-        campoA.classList.add('success');
-        campoB.classList.add('success');
-        msgSucesso.innerHTML = `O valor do campo A ${campoA.value} é menor que o campo B ${campoB.value}`;
-        msgSucesso.style.display = 'block';
-    }
-    else{
-        campoA.classList.add('error');
-        campoB.classList.add('error');
-        msgErro.innerHTML = `O valor do campo A: ${campoB.value} é maior que o campo B: ${campoA.value}`;
-        msgErro.style.display = 'block';
-    }
+    if (campoA.value != campoB.value) {
+        if (campoA.value < campoB.value){
+            campoA.classList.add('success');
+            campoB.classList.add('success');
+            msgSucesso.innerHTML = `O valor do campo A ${campoA.value} é menor que o campo B ${campoB.value}`;
+            msgSucesso.style.display = 'block';
+        }
+        else{
+            campoA.classList.add('error');
+            campoB.classList.add('error');
+            msgErro.innerHTML = `O valor do campo A: ${campoB.value} é maior que o campo B: ${campoA.value}`;
+            msgErro.style.display = 'block';
+        }
+}
 });
 
 campoA.addEventListener('keyup', function(e){
